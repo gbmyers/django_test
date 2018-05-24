5584c41a2f0c698d2d672f596d48afca4d0a5ce8
Geoff <myers.geoffrey@gmail.com>
2018-05-23 21:39:27 -0400

    add stub pages for questions, resutls, voting

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path(r'<int:question_id>/', views.detail, name='detail'),
+    path(r'<int:question_id>/results/', views.results, name='results'),
+    path(r'<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse('You are at the polls index.')
+
+
+def detail(request, question_id):
+    response = f"You're looking at question {question_id}."
+    return HttpResponse(response)
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    response = f"You're voting on question {question_id}."
+    return HttpResponse(response)
