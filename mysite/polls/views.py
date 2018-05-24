5de2ab5196ade5415fd8f06c608b6701882f5f23
Geoff <myers.geoffrey@gmail.com>
2018-05-23 22:53:35 -0400

    implement form on question detail page, allow voting.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -26,5 +27,17 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    response = f"You're voting on question {question_id}."
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        template = 'polls/detail.html'
+        error_message = "You didn't select a choice."
+        context = {'question': question,
+                   'error_message': error_message}
+        return render(request, template, context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',
+                                            args=(question_id,)))
